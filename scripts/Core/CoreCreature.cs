f0731833394a9f10dbc9b1ad94002244955c4572
SavingPot <86786496+SavingPot@users.noreply.github.com>
2023-07-22 10:12:02 +0800

    Delete scripts directory

--- scripts/Core/CoreCreature.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace GameCore
-{
-    public abstract class CoreCreatureProperties<T> where T : CoreCreatureProperties<T>, new()
-    {
-        public static T instance = new T();
-    }
-}
