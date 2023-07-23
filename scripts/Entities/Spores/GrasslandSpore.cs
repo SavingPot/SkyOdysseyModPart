3600cc9263c34387f7c03de17ae75392f503a433
“SavingPot” <bronze_pot@163.com>
2023-07-23 18:42:54 +0800

    modified:   mod_source/biomes/bamboo_grove.json 	modified:   mod_source/biomes/center.json 	modified:   mod_source/biomes/desert.json 	modified:   mod_source/biomes/grassland.json 	modified:   mod_source/biomes/oak_forest.json 	modified:   mod_source/biomes/rain_forest.json 	modified:   mod_source/biomes/savannah_grassland.json 	modified:   mod_source/biomes/snowfield.json 	renamed:    mod_source/entities/item.json -> mod_source/entities/drop.json 	new file:   mod_source/entities/gaurds/desert_guard_sand.json 	new file:   mod_source/entities/gaurds/grassland_guard_storm.json 	modified:   mod_source/scripts/scripts.dll 	modified:   scripts/Entities/Spores/GrasslandSpore.cs

--- scripts/Entities/Spores/GrasslandSpore.cs
+++ scripts/Entities/Spores/GrasslandSpore.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 namespace GameCore
 {
+    [EntityBinding(EntityID.GrasslandSpore)]
     public class GrasslandSpore : Enemy
     {
         protected override void Update()
@@ -11,7 +12,7 @@ namespace GameCore
                 rb.velocity = Tools.GetAngleVector2(transform.position, targetTransform.position).normalized * 5;
             }
         }
-        
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.TryGetComponent<Creature>(out var creature))
