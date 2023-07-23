a3c96587dcd610bfe71f5d0166b4e23060324538
10-cookies <2945576866@qq.com>
2023-07-23 16:40:30 +0800

    new file:   scripts/Entities/Spores/GrasslandSpore.cs

---
+++ scripts/Entities/Spores/GrasslandSpore.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace GameCore
+{
+    public class GrasslandSpore : Enemy
+    {
+        protected override void Update()
+        {
+            if (!targetTransform)
+            {
+                rb.velocity = Tools.GetAngleVector2(transform.position, targetTransform.position).normalized * 5;
+            }
+        }
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.TryGetComponent<Creature>(out var creature))
+            {
+                creature.TakeDamage(10);
+            }
+        }
+    }
+}
