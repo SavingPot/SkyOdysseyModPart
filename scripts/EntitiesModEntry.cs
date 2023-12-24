2f8b876e7f54eba1466c898163f141da9a0be293
“SavingPot” <bronze_pot@163.com>
2023-12-24 11:22:57 +0800

    modified:   mod_source/entities/bullets/extreme_weather_region_guard_sand.json modified:   mod_source/entities/bullets/extreme_weather_region_guard_snowball.json modified:   mod_source/entities/bullets/plant_region_guard_sapling.json modified:   mod_source/entities/bullets/spark.json modified:   mod_source/entities/bullets/wooden_arrow.json modified:   mod_source/entities/drop.json renamed:    mod_source/entities/pig.json -> mod_source/entities/idle_creatures/livestock/pig.json renamed:    mod_source/entities/chicken.json -> mod_source/entities/idle_creatures/livestock/poultry/chicken.json renamed:    mod_source/entities/duck.json -> mod_source/entities/idle_creatures/livestock/poultry/duck.json renamed:    mod_source/entities/caterpillar.json -> mod_source/entities/idle_creatures/small_animals/caterpillar.json renamed:    mod_source/entities/cicada.json -> mod_source/entities/idle_creatures/small_animals/cicada.json renamed:    mod_source/entities/cricket.json -> mod_source/entities/idle_creatures/small_animals/cricket.json renamed:    mod_source/entities/earthworm.json -> mod_source/entities/idle_creatures/small_animals/earthworm.json renamed:    mod_source/entities/fly.json -> mod_source/entities/idle_creatures/small_animals/fly.json renamed:    mod_source/entities/katydid.json -> mod_source/entities/idle_creatures/small_animals/katydid.json renamed:    mod_source/entities/longicorn.json -> mod_source/entities/idle_creatures/small_animals/longicorn.json renamed:    mod_source/entities/maggot.json -> mod_source/entities/idle_creatures/small_animals/maggot.json renamed:    mod_source/entities/mantis.json -> mod_source/entities/idle_creatures/small_animals/mantis.json renamed:    mod_source/entities/pillworm.json -> mod_source/entities/idle_creatures/small_animals/pillworm.json renamed:    mod_source/entities/snail.json -> mod_source/entities/idle_creatures/small_animals/snail.json modified:   mod_source/entities/player.json renamed:    mod_source/entities/guards/extreme_weather_region_guard.json -> mod_source/entities/region_guards/extreme_weather_region_guard.json renamed:    mod_source/entities/guards/plant_region_guard.json -> mod_source/entities/region_guards/plant_region_guard.json modified:   mod_source/recipes/crafting/campfire.json modified:   mod_source/scripts/scripts.dll modified:   mod_source/texture_settings.json modified:   mod_source/textures/entities/enemies/slimes/bamboo_grove/idle.png modified:   mod_source/textures/entities/enemies/slimes/desert/idle.png modified:   mod_source/textures/entities/enemies/slimes/grassland/idle.png modified:   mod_source/textures/entities/enemies/slimes/oak_forest/idle.png modified:   mod_source/textures/entities/enemies/slimes/rain_forest/idle.png modified:   mod_source/textures/entities/enemies/slimes/savannah_grassland/idle.png modified:   mod_source/textures/entities/enemies/slimes/snowfield/idle.png renamed:    mod_source/textures/gui/sidebars/backpack_background.png -> mod_source/textures/gui/sidebars/backpack_inventory_background.png modified:   mod_source/textures/items/slime_gel.png modified:   scripts/EntitiesModEntry.cs

--- scripts/EntitiesModEntry.cs
+++ scripts/EntitiesModEntry.cs
@@ -14,11 +14,14 @@
                 }
             };
 
-            Player.backpackSidebarTable.Add("ori:barrel", (() => { BarrelBlockBehaviour.GenerateItemView().gameObject.SetActive(true); }, () => { BarrelBlockBehaviour.GenerateItemView().gameObject.SetActive(false); }));
-            Player.backpackSidebarTable.Add("ori:cooking_pot", (() => { CookingPotBlockBehaviour.GenerateItemView().gameObject.SetActive(true); }, () => { CookingPotBlockBehaviour.GenerateItemView().gameObject.SetActive(false); }));
-            Player.backpackSidebarTable.Add("ori:soft_clay_furnace", (() => { SoftClayFurnace.GenerateItemView().gameObject.SetActive(true); }, () => { SoftClayFurnace.GenerateItemView().gameObject.SetActive(false); }));
-            Player.backpackSidebarTable.Add("ori:wooden_chest", (() => { WoodenChest.GenerateItemView().gameObject.SetActive(true); }, () => { WoodenChest.GenerateItemView().gameObject.SetActive(false); }));
-            //Player.backpackSidebarTable.Add("ori:wooden_bowl_with_water", (() => { WoodenBowlWithWaterBehaviour.GenerateItemView().gameObject.SetActive(true); }, () => { WoodenBowlWithWaterBehaviour.GenerateItemView().gameObject.SetActive(false); }));
+            PlayerCenter.OnAddPlayer += caller =>
+            {
+                caller.backpackSidebarTable.Add("ori:barrel", (() => { BarrelBlockBehaviour.GenerateItemView().gameObject.SetActive(true); }, () => { BarrelBlockBehaviour.GenerateItemView().gameObject.SetActive(false); }));
+                caller.backpackSidebarTable.Add("ori:cooking_pot", (() => { CookingPotBlockBehaviour.GenerateItemView().gameObject.SetActive(true); }, () => { CookingPotBlockBehaviour.GenerateItemView().gameObject.SetActive(false); }));
+                caller.backpackSidebarTable.Add("ori:soft_clay_furnace", (() => { SoftClayFurnace.GenerateItemView().gameObject.SetActive(true); }, () => { SoftClayFurnace.GenerateItemView().gameObject.SetActive(false); }));
+                caller.backpackSidebarTable.Add("ori:wooden_chest", (() => { WoodenChest.GenerateItemView().gameObject.SetActive(true); }, () => { WoodenChest.GenerateItemView().gameObject.SetActive(false); }));
+                //caller.backpackSidebarTable.Add("ori:wooden_bowl_with_water", (() => { WoodenBowlWithWaterBehaviour.GenerateItemView().gameObject.SetActive(true); }, () => { WoodenBowlWithWaterBehaviour.GenerateItemView().gameObject.SetActive(false); }));
+            };
 
             GScene.AfterChanged += scene =>
             {
