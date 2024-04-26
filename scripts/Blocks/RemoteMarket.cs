7fe5908a943cd64c952f4a48ff9fb9ae7dbbc55f
“SavingPot” <bronze_pot@163.com>
2024-04-26 23:26:23 +0800

    deleted:    mod_source/blocks/boundary_gate.json new file:   mod_source/blocks/remote_market.json modified:   mod_source/items/berry.json modified:   mod_source/items/onion.json modified:   mod_source/langs/zh_cn.json new file:   mod_source/recipes/crafting/remote_market.json modified:   mod_source/scripts/scripts.dll modified:   mod_source/texture_settings.json deleted:    mod_source/textures/blocks/boundary_gate.png new file:   mod_source/textures/blocks/remote_market.png deleted:    scripts/Blocks/BoundaryGate.cs new file:   scripts/Blocks/RemoteMarket.cs

---
+++ scripts/Blocks/RemoteMarket.cs
@@ -0,0 +1,21 @@
+using SP.Tools.Unity;
+using UnityEngine;
+using static GameCore.PlayerUI;
+
+namespace GameCore
+{
+    public class RemoteMarket : Block
+    {
+        public override bool PlayerInteraction(Player player)
+        {
+            if (player.TryGetUsingItem(out var item) && item.data.economy.worth != 0)
+            {
+                player.ServerReduceUsingItemCount();
+                player.AddCoin(item.data.economy.worth);
+                return true;
+            }
+
+            return base.PlayerInteraction(player);
+        }
+    }
+}
