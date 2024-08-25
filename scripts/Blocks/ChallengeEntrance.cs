a72e4a0371cfb016d7997c0f9375ff3116da8acb
“SavingPot” <bronze_pot@163.com>
2024-08-25 21:36:20 +0800

    modified:   mod_source/entities/biome_guards/desert_guard.json modified:   mod_source/entities/biome_guards/snowfield_guard.json modified:   mod_source/langs/zh_cn.json modified:   mod_source/scripts/scripts.dll modified:   scripts/Blocks/ChallengeEntrance.cs modified:   scripts/Blocks/SleepingPortal.cs

--- scripts/Blocks/ChallengeEntrance.cs
+++ scripts/Blocks/ChallengeEntrance.cs
@@ -1,7 +1,25 @@
+using SP.Tools.Unity;
+using System.Collections;
+using UnityEngine;
+
 namespace GameCore
 {
     public class ChallengeEntrance : Block
     {
         //TODO: 等待其他玩家加入
+        protected bool hasChallengeBegun;
+
+        protected virtual void StartChallenge(Player player, string challengeId)
+        {
+            hasChallengeBegun = true;
+            player.GenerateChallengeRoom(challengeId);
+            CoroutineStarter.Do(WaitForChallengeRoom(player));
+        }
+
+        protected virtual IEnumerator WaitForChallengeRoom(Player player)
+        {
+            while (player.regionIndex.y != ChallengeRoomGeneration.challengeRoomIndexY)
+                yield return null;
+        }
     }
 }
--- scripts/Blocks/SleepingPortal.cs
+++ scripts/Blocks/SleepingPortal.cs
@@ -1,39 +1,85 @@
 using System.Collections;
+using System.Linq;
 using SP.Tools.Unity;
 using UnityEngine;
 
 namespace GameCore
 {
-    public class SleepingPortal : ChallengeEntrance
+    public sealed class SleepingPortal : ChallengeEntrance
     {
         string GetGuardId() => $"{GFiles.world.GetOrAddRegion(chunk.regionIndex).biomeId}_guard";
+
+
 
         //TODO: 击败后把方块变成 ActivatedPortal
         public override bool PlayerInteraction(Player player)
         {
+            if (hasChallengeBegun)
+                return false;
+
+            //检查 x轴
             if (chunk.regionIndex.x == 0)
             {
                 Debug.LogError($"沉睡的传送门不应出现在中心区域");
                 return true;
             }
 
+            //检查守护者
             if (ModFactory.CompareEntity(GetGuardId()) == null)
             {
                 Debug.LogError($"守护者召唤失败：无法匹配到实体 {GetGuardId()}");
                 return true;
             }
 
-            player.GenerateChallengeRoom("");
-            CoroutineStarter.Do(WaitForEnteringTheRoom(player));
+            //开始挑战
+            StartChallenge(player, null);
             return true;
         }
 
-        IEnumerator WaitForEnteringTheRoom(Player player)
+        protected override IEnumerator WaitForChallengeRoom(Player player)
         {
-            while (player.regionIndex.y != ChallengeRoomGeneration.challengeRoomIndexY)
-                yield return null;
+            yield return base.WaitForChallengeRoom(player);
 
-            GM.instance.SummonEntity(Region.GetMiddle(player.regionIndex), GetGuardId());
+            //生成守护者
+            GM.instance.SummonEntityCallback(Region.GetMiddle(player.regionIndex), GetGuardId(), entity => CoroutineStarter.Do(WaitForChallengeEnd(player, (BiomeGuard)entity)));
+        }
+
+        IEnumerator WaitForChallengeEnd(Player player, BiomeGuard guard)
+        {
+            while (player.regionIndex.y == ChallengeRoomGeneration.challengeRoomIndexY)
+            {
+                //若玩家死亡那么不等了
+                if (player.isDead)
+                {
+                    hasChallengeBegun = false;
+                    yield break;
+                }
+
+                //若守护者死亡，则挑战成功
+                if (guard.isDead) guard = null;
+                if (guard == null)
+                {
+                    //送玩家回到原本的区域
+                    var portalRegion = PosConvert.MapPosToRegionIndex(pos);
+                    player.GenerateRegion(portalRegion, true);
+
+                    //等待区域生成
+                    while (!GM.instance.generatedExistingRegions.Any(r => r.index == portalRegion))
+                        yield return null;
+
+                    //激活传送门
+                    Map.instance.SetBlockNet(pos, isBackground, BlockID.Portal, null);
+                    yield break;
+                }
+                //如果还在挑战那么继续等待
+                else
+                {
+                    yield return null;
+                }
+            }
+
+            //玩家意外离开了挑战区
+            hasChallengeBegun = false;
         }
     }
 }
